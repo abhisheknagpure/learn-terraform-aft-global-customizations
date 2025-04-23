a3b853b55afec3d803f205a2f9e60db0b5e8ea3f
abhisheknagpure <110444541+abhisheknagpure@users.noreply.github.com>
2025-04-23 14:04:49 +0530

    update accounts

--- terraform/local.tf
+++
@@ -1,9 +0,0 @@
-locals {
-has_permissions_boundary = var.permissions_boundary_arn != ""
-snapshot_scanning_enabled = var.enable_snapshot_scanning == "Yes"
-auto_remediation_enabled = var.integration_mode == "DETECT_AND_REMEDIATE"
-is_cds_integration_enabled = var.enable_cds_integration == "Yes"
-is_organization_onboarding_selected = var.is_organization_onboarding == "Yes"
-has_excluded_accounts = var.is_organization_onboarding == "Yes" && var.excluded_accounts != ""
-has_no_excluded_accounts = var.is_organization_onboarding == "Yes" && var.excluded_accounts == ""
-}
